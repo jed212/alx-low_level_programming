9953e0aa0e9fd24e773842d669100fb54eea9d1c
jed212 <jedidahfavour@gmail.com>
2023-07-13 14:27:04 -0700

    C program that prints a sentence followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * main - uses puts to print  "Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual
+ *  puzzle
  *
  * The program returns 0 to indicate successful execution
  *
