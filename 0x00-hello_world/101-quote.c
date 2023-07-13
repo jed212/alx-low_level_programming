f66d6b8e436ee7f7d65204854203852a40be8a8e
jed212 <jedidahfavour@gmail.com>
2023-07-13 15:29:56 -0700

    C program that print a sentence followed by new line without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ *
+ * Return: Always 1(success)
+ *
+ */
+
+int main(void) 
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, message, strlen(message));
+
+    return (1);
+}
+
