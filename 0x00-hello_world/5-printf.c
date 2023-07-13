57d4393acd560233c386d4849002c3247c45e216
jed212 <jedidahfavour@gmail.com>
2023-07-13 14:35:44 -0700

    C program that prints a sentence using printf, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *  main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," using printf
+ *                followed by a new line
+ * Return :Always 0
+ */
+
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
