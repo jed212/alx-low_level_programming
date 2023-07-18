9f698653e8e0400d717e8059ab40dcb8def56da8
jed212 <jedidahfavour@gmail.com>
2023-07-18 11:56:39 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,12 @@
 void print_alphabet_x10(void)
 {
 	char i;
-	int j;
+	int j = 0;
 
-	for (i = 'a'; i <= 'z'; i++)
+	while (j++ <= 9)
 	{
-		for (j = 0; j <= 10; j++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
