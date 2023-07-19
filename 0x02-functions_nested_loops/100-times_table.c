16e93a95815a01939753db862269b7cc27f3dc6e
jed212 <jedidahfavour@gmail.com>
2023-07-18 23:34:29 -0700

    fix errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,14 @@ void print_times_table(int n)
 	{
 		for (num = 0; num <= n; num++)
 		{
-			for (mult = 0; mult <= n; mult++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				prod = num * mult;
+				_putchar(',');
 				_putchar(' ');
+
+				prod = num * mult;
 
 				if (prod <= 99)
 					_putchar(' ');
