e347de1b1ed342e516d07f20f10a10686faa2184
jed212 <jedidahfavour@gmail.com>
2023-07-20 13:44:58 -0700

    fixes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,13 @@
  * _isupper - Checks if a character is uppercase
  * @c: The character being checked
  *
- * Return: Returns 1 if c is upper 0 otherwise
+ * Return: 1 if c is upper 0 otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (0);
+
 	else
 		return (1);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,11 @@
 
 /**
  * File: main.h
- * Desc: Contains header files for the 0x04-more_functions_nested_loops project
+ * Desc: Contains header files for the
+ * 0x04-more_functions_nested_loops project
  *
  */
+
 int _putchar(char c);
 int _isupper(int c);
 
