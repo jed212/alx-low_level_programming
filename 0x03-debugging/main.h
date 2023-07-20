01c69a2d24f9a70712cca90880088644ffd57b43
jed212 <jedidahfavour@gmail.com>
2023-07-20 06:33:40 -0700

    debug program prints the largest of three integers.

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,8 +6,8 @@
 /**
  * File: main.h
  * Desc: File containing header files for the 0x03-debugging project
- * @i: the number to be checked
  */
-void positive_or_negative(int i);
+void positive_or_negative(int);
+int largest_number(int, int, int);
 
 #endif /* MAIN_H */
