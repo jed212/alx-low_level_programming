64cd1f65cdd39e39b153541895c16cb175d68d4c
jed212 <jedidahfavour@gmail.com>
2023-07-24 06:46:30 -0700

    add header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: Contains prototypes for the 0x05-pointers_arrays_strings project
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
