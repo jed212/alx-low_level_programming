a75c5f63e96f399bd636c3790209fddb79d7d830
jed212 <jedidahfavour@gmail.com>
2023-07-27 01:58:05 -0700

    function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ * @s: the string
+ *
+ * Return: Returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strncat - concatenates n characters of string src to the end of dest
+ * @dest: the initial string
+ * @src: the strig to be concatenated to dest
+ *
+ * Return: Returns the pointer to the initial string 
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, length0, length1;
+
+	i = 0;
+	length0 = _strlen(dest);
+	length1 = _strlen(src);
+	while (i < n)
+	{
+		if (i == length1)
+			break;
+		*(dest + length0 + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@
  * Desc: Contains prototypes for the 0x06-pointers_arrays_strings project
  */
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
