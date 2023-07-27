7586043b18c8122fd9e234d9fdd2239380c157e4
jed212 <jedidahfavour@gmail.com>
2023-07-27 03:20:49 -0700

    fix betty error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,8 +20,8 @@ int _strlen(char *s)
  * _strncpy - takes a string and copies n characters of it to a buffer
  * @dest: the initial string
  * @src: the string to be copied to dest
- *@n: the number of characters to be copied
-
+ * @n: the number of characters to be copied
+ *
  * Return: returns the pointer to the initial tring dest
  */
 char *_strncpy(char *dest, char *src, int n)
