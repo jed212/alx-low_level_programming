237434c4234d7b40e6335b7314a6920b6d3968e0
jed212 <jedidahfavour@gmail.com>
2023-07-27 04:25:53 -0700

    function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,20 +5,19 @@
  * @s1: First string
  * @s2: Second string
  *
- * Return: Returns integer sum of the difference,
- * betwen each characters'ASCII numbers.
+ * Return: Returns integer sum of the difference betwen
+ * each characters'ASCII values at the current position.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, diff;
-
-	i = 0;
-	diff = 0;
-
-	while (*(s1 + i) != '\0' || *(s2 + i) != '\0')
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		diff = *(s1 + i) - *(s2 + i);
-		i++;
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-	return (diff);
+	return (*s1 - *s2);
 }
