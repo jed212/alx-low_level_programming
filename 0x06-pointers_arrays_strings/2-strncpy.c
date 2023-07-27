1d57c6f680a8f8b1127f08bd8433f1d809940014
jed212 <jedidahfavour@gmail.com>
2023-07-27 03:16:25 -0700

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ * @s: the string
+ *
+ * Return: returns the length of the string s
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strncpy - takes a string and copies n characters of it to a buffer
+ * @dest: the initial string
+ * @src: the string to be copied to dest
+ *@n: the number of characters to be copied
+
+ * Return: returns the pointer to the initial tring dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
