98b2c84456462289d42f80c1e1209445f4f3691c
jed212 <jedidahfavour@gmail.com>
2023-07-31 10:36:05 -0700

    add header file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: contains prototypes for the 0x07-pointers_arrays_strings project
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
