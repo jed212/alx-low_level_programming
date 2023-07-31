a477c6e5647bf1280b6e727cc4dd2f98906461b6
jed212 <jedidahfavour@gmail.com>
2023-07-31 12:01:39 -0700

    add function prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@
  * Desc: contains prototypes for the 0x07-pointers_arrays_strings project
  */
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
