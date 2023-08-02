0f26ca8ff692afe4f7c131743d2258122552733a
jed212 <jedidahfavour@gmail.com>
2023-08-02 11:05:10 -0700

    add main.h file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: contains prototypes for the 0x08 Recursion project files
+ */
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
