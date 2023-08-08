053064fbe909e22272d6416b27bcdeca73dee4c9
jedidiah <jedidahfavour@gmail.com>
2023-08-08 19:34:15 +0300

    add main header file

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Desc: contains function prototypes for 0x0B-malloc_free projects
+ */
+char *create_array(unsigned int size, char c);
+
+#endif
