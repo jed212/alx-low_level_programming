63e0ed8158a130f0dc83eb548830b00c8630648c
jedidiah <jedidahfavour@gmail.com>
2023-08-14 15:03:18 +0300

    add description for x

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,7 +5,7 @@
  * File: 3-function_like_macro.h
  * Desc: a function-like macro ABS(x) that computes
  * the absolute value of a number x
+ * @x: the number argument
  */
-
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #endif
