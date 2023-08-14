3caa20c02ef3b59dc6de149a7f875ae41036fafe
jedidiah <jedidahfavour@gmail.com>
2023-08-14 15:18:30 +0300

    fix betty-style error

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,7 +3,8 @@
 
 /**
  * File: 4-sum.h
- * Desc:  a function-like macro SUM(x, y) that computes the sum of the numbers x and y.
+ * Desc:  a function-like macro SUM(x, y) that
+ * computes the sum of the numbers x and y.
  * @x: First number argument
  * @y: Second number argument
  */
