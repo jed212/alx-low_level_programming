1ff31ef501c306464bfbd43e3ff4e0b602e5e625
jedidiah <jedidahfavour@gmail.com>
2023-08-17 09:51:48 +0300

    fix header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,10 +2,6 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 
-/**
- * File: function_pointers.h
- * Desc: contains prototypes for the project
- */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
