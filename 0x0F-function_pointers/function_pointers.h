a329b82aea82ea5a518223005cfb1620e87dded6
jedidiah <jedidahfavour@gmail.com>
2023-08-17 09:48:20 +0300

    add comments

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
+
+/**
+ * File: function_pointers.h
+ * Desc: contains prototypes for the project
+ */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
