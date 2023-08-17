ca010ec4012e8ea0ede1edc94dccc7b911aa5ccc
jedidiah <jedidahfavour@gmail.com>
2023-08-17 09:38:32 +0300

    add header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * File: function_pointers.h
+ * Desc: contains prototypes for the 0x0F-function_pointers project
+ */
+void print_name(char *name, void (*f)(char *));
+#endif
